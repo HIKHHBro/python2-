f0fdc9accaff81be75fdd426b2f510abd80d7bc8
haikangpingtouge <w1121508831@gmail.com>
2020-06-22 01:35:28 +0800

    安装脚本

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# python2-
+# python2.7.5安装包和依赖包
+** install.bat 安装脚本
---
+++ install.bat
@@ -0,0 +1,6 @@
+rem %~dp0% 表示当前文件夹
+rem /d  跳转
+rem xlwt-1.3.0  包文件名
+cd /d %~dp0%xlwt-1.3.0 
+python2  setup.py install
+pause
